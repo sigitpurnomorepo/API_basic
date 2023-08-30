55a73e8e6f99eccf5ab776470b3598763ab4a2c5
sigit purnomo <sigitoffice.jkt@gmail.com>
2023-08-30 11:21:18 +0700

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from app.user import userbp
 
-from config import Config
 
-def create_app(config_class = Config):
+def create_app():
     app = Flask(__name__)
     # existing code omitted
     # Memanggil konfigurasi untuk digunakan oleh flask
-    app.config.from_object(config_class)
 
-    
     app.register_blueprint(userbp)
     return app
 
